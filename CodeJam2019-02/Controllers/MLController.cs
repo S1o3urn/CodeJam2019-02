bfa98be4a8c54f7e10a92e29780a7b6cf6f2d92a
s1o3urn <d4vid4pplepie@gmail.com>
2019-11-16 12:13:04 -0500

    Set up tasks

--- CodeJam2019-02/Controllers/MLController.cs
+++ CodeJam2019-02/Controllers/MLController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeJam2019_02.Controllers
@@ -15,6 +17,7 @@ namespace CodeJam2019_02.Controllers
         /// Trains the preprocessed data
         public IActionResult TrainModel()
         {
+            //TODO: Currently not implemented
             return View();
         }
 
@@ -22,6 +25,35 @@ namespace CodeJam2019_02.Controllers
         public IActionResult PredictSpending()
         {
             return View();
+        }
+
+        /// <summary>
+        /// This method takes in the uploaded test files and preprocess them for prediction
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult ProcessData(List<IFormFile> files)
+        {
+            //TODO: Store files into SQL database
+            foreach (var file in files)
+            {
+                // Create dataTable to hold csv file
+                DataTable csvData = new DataTable();
+
+                //Read csv into dataTable
+                try
+                {
+                    
+                }
+                catch(Exception BadInputFile)
+                {
+                    return null;
+                }
+            }
+            //TODO: Preprocess all three tables
+
+            return Ok();
         }
     }
 }
