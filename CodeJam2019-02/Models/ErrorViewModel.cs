6da75af3bf663ef68c259d0afc8a8ca5e1028346
s1o3urn <d4vid4pplepie@gmail.com>
2019-11-15 20:20:40 -0500

    Add project files.

---
+++ CodeJam2019-02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeJam2019_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
